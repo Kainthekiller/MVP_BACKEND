b23f4270e3b91c6b72ce635d8bd91343b7f755fa
kainthekiller <phillipyates12@hotmail.com>
2022-10-31 12:00:58 -0500

    Patch Finished CRUD Should be good to go from this point On

--- src/main/java/PMCS_MVP/PMCS_MVP/Controller/PMCSController.java
+++ src/main/java/PMCS_MVP/PMCS_MVP/Controller/PMCSController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -36,6 +38,43 @@ public class PMCSController {
     {
         return pmcsRepo.findAll();
     }
+
+    //GET ONE
+    @GetMapping("{id}")
+    ResponseEntity<PMCS> getOnePMCS(@PathVariable Long id)
+    {
+        if (pmcsRepo.findById(id).isPresent())
+        {
+            return new ResponseEntity<>(pmcsRepo.findById(id).get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    //UPDATE
+    @PatchMapping("{id}")
+    ResponseEntity<PMCS> patchOnePMCS(@PathVariable Long id, @RequestBody Map<String, String> body)
+    {
+        if(pmcsRepo.findById(id).isPresent())
+        {
+            PMCS holder = pmcsRepo.findById(id).get();
+            for (Map.Entry<String, String> entry: body.entrySet())
+            {
+                switch (entry.getKey())
+                {
+                    case  "itemName" -> holder.setItemName(entry.getValue());
+                    case  "publishDate" -> holder.setPublishDate(LocalDate.parse(entry.getValue()));
+                    case  "tmNumber" -> holder.setTmNumber(entry.getValue());
+                    case  "pageCount" -> holder.setPageCount(Integer.valueOf(entry.getValue()));
+                    case   "pagePmcsStart" -> holder.setPagePmcsStart(Integer.valueOf(entry.getValue()));
+                }
+            }
+            return new ResponseEntity<>(pmcsRepo.save(holder), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+
 
 
 }
