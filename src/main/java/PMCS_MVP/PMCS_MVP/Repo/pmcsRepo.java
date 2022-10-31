f4ac0cd2d888984e7cfa49a0341a3e5dff0dac46
kainthekiller <phillipyates12@hotmail.com>
2022-10-31 11:09:52 -0500

    Test

--- src/main/java/PMCS_MVP/PMCS_MVP/Repo/pmcsRepo.java
+++ src/main/java/PMCS_MVP/PMCS_MVP/Repo/pmcsRepo.java
@@ -4,4 +4,5 @@ import PMCS_MVP.PMCS_MVP.Model.pmcs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface pmcsRepo extends JpaRepository<pmcs, Long> {
+    String test = "";
 }
