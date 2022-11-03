d1d526270bf6d3b009ba94188a0f3fb2cf92e2ba
kainthekiller <phillipyates12@hotmail.com>
2022-11-03 12:35:47 -0500

    Added Get ALl To Controller

--- src/main/java/PMCS_MVP/PMCS_MVP/Controller/AccountController.java
+++ src/main/java/PMCS_MVP/PMCS_MVP/Controller/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/Account")
@@ -42,6 +44,12 @@ public class AccountController {
         }
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 
+    }
+
+    @GetMapping("")
+    List<Account> getAllUsers()
+    {
+        return accountRepo.findAll();
     }
 
 
